b420f8bd288906623939852bd58dcf3bfa2bf265
Fabio Fujii <fujii.kimio.k@gmail.com>
2022-10-28 12:59:03 -0300

    adding comment for rainfall class

--- src/preprocess/transformers.py
+++ src/preprocess/transformers.py
@@ -2,7 +2,11 @@ import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
 
 class RainfallInitialPreprocess(BaseEstimator, TransformerMixin):
-    
+    """
+    Initial preprocess step necessary for rainfall data.
+    This class transforms date into datetime and extracts month and year from
+    the date.
+    """
     def fit(self, X, y=None):
         return self
     
